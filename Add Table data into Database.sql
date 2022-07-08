1464ac443e11168f558f865f8eb73795e211ba39
jpnoro2003 <jpnoro2003@gmail.com>
2022-07-07 23:11:06 -0400

    Add files via upload

---
+++ Add Table data into Database.sql
@@ -0,0 +1,2 @@
+Insert into TEST (Pts, Reb, Ast, Stl, Blk, Tov, PF, [FG%], [3PT%], [FT%], [TS%], PER, [3PAr], FTr, [TRB%], [AST%], [STL%], [BLK%], [TOV%], [USG%])
+select * from [2122-posavg]
---
+++ Update null values.sql
@@ -0,0 +1,5 @@
+update nba1314
+SET [3p%] = 0
+where [3p%] is NULL
+-- For basic analytics: [fg%], [ft%], [3p%], [efg%], [2p%], replace with 0
+-- For advanced analytics: [ts%], [3par], [ftr], [tov%], replace with 0
