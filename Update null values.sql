dd5c35cdd0546eb89438c3c01f8353e9fcba1eb6
jpnoro2003 <jpnoro2003@gmail.com>
2022-07-27 17:19:04 -0400

    Add files via upload

--- Update null values.sql
+++ Update null values.sql
@@ -1,5 +1,5 @@
-update nba1314
+update nba7980
 SET [3p%] = 0
-where [3p%] is NULL
--- For basic analytics: [fg%], [ft%], [3p%], [efg%], [2p%], replace with 0
+where [3p%] is NULL;
+-- For basic analytics: [fg%], [ft%], [3p%], [2p%], replace with 0
 -- For advanced analytics: [ts%], [3par], [ftr], [tov%], replace with 0
